d6b6ca183245e54ad71e7122dc84aadb639c976a
Raghu Betina <rbetina@gmail.com>
2013-11-01 15:27:03 -0500

    Added a route for the form for a new picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rxngif::Application.routes.draw do
+  get("/new_picture_form", { :controller => "pictures", :action => "new" })
   get("/all_pictures", { :controller => "pictures", :action => "index" })
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
 end
