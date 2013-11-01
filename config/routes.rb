48df473c60e5a086e67416f2836e58d38599b62b
Raghu Betina <rbetina@gmail.com>
2013-11-01 15:22:24 -0500

    Added a route for the list of all pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rxngif::Application.routes.draw do
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
 end
