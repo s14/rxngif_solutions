7f5322a8e7da964e4acca77955de7fa6590f3060
Raghu Betina <rbetina@gmail.com>
2013-11-01 15:52:25 -0500

    Added a route to create a picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rxngif::Application.routes.draw do
+  get("/create_picture", { :controller => "pictures", :action => "create" })
   get("/new_picture_form", { :controller => "pictures", :action => "new" })
   get("/all_pictures", { :controller => "pictures", :action => "index" })
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
